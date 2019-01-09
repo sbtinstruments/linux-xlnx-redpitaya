073edf94cafb088bdb228a8bce620d1552282fe9
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-01-09 15:43:18 +0100

    Add FB_TFT driver for the ILI9488 display controller. Note that FB_TFT's "fbtft_write_vmem16_bus8" function has been replaced with a patched version to account for the RGB666 format. Add FB_TFT driver for the P220MD-T display panel that uses the ILI9488 display controller.

--- drivers/staging/fbtft/fbtft.h
+++ drivers/staging/fbtft/fbtft.h
@@ -276,6 +276,7 @@ int fbtft_write_gpio16_wr_latched(struct fbtft_par *par, void *buf, size_t len);
 int fbtft_write_vmem8_bus8(struct fbtft_par *par, size_t offset, size_t len);
 int fbtft_write_vmem16_bus16(struct fbtft_par *par, size_t offset, size_t len);
 int fbtft_write_vmem16_bus8(struct fbtft_par *par, size_t offset, size_t len);
+int fbtft_write_vmem16_bus8_patched(struct fbtft_par *par, size_t offset, size_t len);
 int fbtft_write_vmem16_bus9(struct fbtft_par *par, size_t offset, size_t len);
 void fbtft_write_reg8_bus8(struct fbtft_par *par, int len, ...);
 void fbtft_write_reg8_bus9(struct fbtft_par *par, int len, ...);

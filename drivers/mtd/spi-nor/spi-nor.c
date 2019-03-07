3fc8931d2d00f38ee8c45f5e48d30a84c37091f1
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-03-07 14:05:12 +0100

    spi-nor: add CONFIG_MTD_SPI_NOR_JEDEC_ID_CHECK
    The flash controller itself contains a JEDEC ID that can be used
    to infer information about the controller. Alternatively, the user
    can provide a name that the driver will match against a list of known
    flash controllers.
    
    If given a name, the driver will verify that the name corresponds to
    the JEDEC ID read from the flash controller. This is generally a good
    practice. If the flash controller is not physically connected at this
    point, however, then the check will fail. Disable this check to allow
    the flash controller to be added even if it is not yet physically
    connected to the system.

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1742,6 +1742,7 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 	if (IS_ERR_OR_NULL(info))
 		return -ENOENT;
 
+#ifdef CONFIG_MTD_SPI_NOR_JEDEC_ID_CHECK
 	/*
 	 * If caller has specified name of flash model that can normally be
 	 * detected using JEDEC, let's verify it.
@@ -1765,6 +1766,7 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 			info = (struct flash_info *)jinfo;
 		}
 	}
+#endif
 
 	mutex_init(&nor->lock);
 
